505d728363dcb7a682291978f0918559df1fe9c2
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-10-19 05:53:48 +0100

    working on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("Good Morning"),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
