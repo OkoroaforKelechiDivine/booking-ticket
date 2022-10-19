ea41d68d4c24ca14baf31d122bb28d7539cef994
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-10-19 04:49:58 +0100

    Activated debug service listening

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -9,17 +9,32 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+
+  int _selectedIndex = 1;
+  static final List<Widget>_widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile")
+  ];
+
+  void _onItemTapped(int index){
+    _selectedIndex = index;
+    print("${_selectedIndex}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My tickets")
       ),
-      body: const Center(
-        child: Text("My body"),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
       ),
 
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
         elevation: 10,
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -28,8 +43,8 @@ class _BottomBarState extends State<BottomBar> {
         items: const [
           BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled), label: "Home"),
           BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular), activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_airplane_regular), activeIcon: Icon(FluentSystemIcons.ic_fluent_airplane_filled), label: "Airplane"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular), activeIcon: Icon(FluentSystemIcons.ic_fluent_person_add_filled), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular), activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled), label: "Airplane"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular), activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled), label: "Profile"),
         ],
       ),
     );
